fd2a52472a019eac888a4de48f9a4c54b1338501
Lazy-Chowhound <1103036472@qq.com>
2022-01-11 16:46:37 +0800

    增加查询扰动概况和扰动详情Controller

---
+++ audio_java/src/main/java/szp/audio/audio_java/NoisePatternController.java
@@ -0,0 +1,44 @@
+package szp.audio.audio_java;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Nakano Miku
+ */
+@RestController
+public class NoisePatternController {
+
+    @Autowired
+    private RpcUtil rpcUtil;
+
+    /**
+     * 获取扰动概况
+     */
+    @RequestMapping("/noisePatternSummary")
+    public Result getNoisePatternSummary(@RequestParam(value = "dataset") String dataset) {
+        try {
+            return Result.success(StatusCode.SUCCESS.getStatus(),
+                    rpcUtil.sendRequest("getNoisePatternSummary", dataset));
+        } catch (XmlRpcException xmlRpcException) {
+            return Result.fail(StatusCode.FAIL.getStatus(), xmlRpcException.getMessage());
+        }
+    }
+
+    /**
+     * 获取扰动详情
+     */
+    @RequestMapping("/noisePatternDescription")
+    public Result getNoisePatternDetail(@RequestParam(value = "dataset") String dataset,
+                                        @RequestParam(value = "patternType") String patternType) {
+        try {
+            return Result.success(StatusCode.SUCCESS.getStatus(),
+                    rpcUtil.sendRequest("getNoisePatternDetail", dataset, patternType));
+        } catch (XmlRpcException xmlRpcException) {
+            return Result.fail(StatusCode.FAIL.getStatus(), xmlRpcException.getMessage());
+        }
+    }
+}
