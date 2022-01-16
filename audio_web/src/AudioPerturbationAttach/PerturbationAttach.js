e5b4be727a5f8c6247f280f51e3f9e984e1b55a4
Lazy-Chowhound <1103036472@qq.com>
2022-01-16 13:59:38 +0800

    条件渲染 添加结果页

--- audio_web/src/AudioPerturbationAttach/PerturbationAttach.js
+++ audio_web/src/AudioPerturbationAttach/PerturbationAttach.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Modal, Progress, Table} from "antd";
+import {Button, Modal, Progress, Result, Table} from "antd";
 import PatternDisplay from "./PatternDisplay";
 import {CloudUploadOutlined} from "@ant-design/icons";
 import "../css/PerturbationAttach.css"
@@ -12,7 +12,8 @@ class PerturbationAttach extends React.Component {
             dataSource: [],
             patternChoices: [],
             percent: 30,
-            visible: false
+            visible: false,
+            operationDone: false
         };
     }
 
@@ -100,6 +101,10 @@ class PerturbationAttach extends React.Component {
                     visible: true
                 })
                 // todo 发送ajax setstate
+
+                this.setState({
+                    operationDone: true
+                })
             }
         }
 
@@ -122,39 +127,56 @@ class PerturbationAttach extends React.Component {
                 Table.SELECTION_NONE
             ],
         };
+
+        let content;
+        if (this.state.operationDone) {
+            content = <Result status="success" title="Add Noise pattern Successfully!"
+                              subTitle={`You have add or change ${this.state.selectedRowKeys.length} noise patterns`}
+                              extra={[
+                                  <Button type="primary" key="add"><a href={"/perturbationAttach"}>Add Again</a></Button>,
+                                  <Button key="detail"><a href={"/perturbationDisplay"}>See Detail</a></Button>,
+                              ]}
+            />
+        } else {
+            content =
+                <div>
+                    <Table rowSelection={rowSelection} columns={this.columns}
+                           dataSource={this.state.dataSource} locale={locales}
+                           summary={() => (
+                               <Table.Summary fixed>
+                                   <Table.Summary.Row>
+                                       <Table.Summary.Cell index={0}>Summary</Table.Summary.Cell>
+                                       <Table.Summary.Cell index={1}>
+                                           <div style={{textAlign: "center"}}>{selectedRowKeys.length >= 10 ?
+                                               `${selectedRowKeys.length} items selected / total ${this.state.dataSource.length} items` :
+                                               `${selectedRowKeys.length} items selected / total ${this.state.dataSource.length} items`}
+                                           </div>
+                                       </Table.Summary.Cell>
+                                       <Table.Summary.Cell index={2}>
+                                           <div style={{textAlign: "center"}}>
+                                               <Button type="primary" shape="round" icon={<CloudUploadOutlined/>}
+                                                       onClick={() => {
+                                                           this.handleClick()
+                                                       }}>
+                                                   确认提交
+                                               </Button>
+                                           </div>
+                                       </Table.Summary.Cell>
+                                   </Table.Summary.Row>
+                               </Table.Summary>
+                           )}
+                    />
+                    <Modal title={"处理中......"} visible={this.state.visible} footer={null} width={400}
+                           onCancel={this.handleCancel}>
+                        <div style={{textAlign: "center"}}>
+                            <Progress type="circle" percent={this.state.percent}/>
+                        </div>
+                    </Modal>
+                </div>
+        }
         return (
             <div>
-                <Table rowSelection={rowSelection} columns={this.columns}
-                       dataSource={this.state.dataSource} locale={locales}
-                       summary={() => (
-                           <Table.Summary fixed>
-                               <Table.Summary.Row>
-                                   <Table.Summary.Cell index={0}>Summary</Table.Summary.Cell>
-                                   <Table.Summary.Cell index={1}>
-                                       <div style={{textAlign: "center"}}>{selectedRowKeys.length >= 10 ?
-                                           `${selectedRowKeys.length} items selected / total ${this.state.dataSource.length} items` :
-                                           `${selectedRowKeys.length} items selected / total ${this.state.dataSource.length} items`}
-                                       </div>
-                                   </Table.Summary.Cell>
-                                   <Table.Summary.Cell index={2}>
-                                       <div style={{textAlign: "center"}}>
-                                           <Button type="primary" shape="round" icon={<CloudUploadOutlined/>}
-                                                   onClick={() => {
-                                                       this.handleClick()
-                                                   }}>
-                                               确认提交
-                                           </Button></div>
-                                   </Table.Summary.Cell>
-                               </Table.Summary.Row>
-                           </Table.Summary>
-                       )}
-                />
-                <Modal title={"处理中......"} visible={this.state.visible} footer={null}
-                       width={400}
-                       onCancel={this.handleCancel}>
-                    <div style={{textAlign: "center"}}>
-                        <Progress type="circle" percent={this.state.percent}/></div>
-                </Modal>
+                {content}
             </div>
         );
     }
