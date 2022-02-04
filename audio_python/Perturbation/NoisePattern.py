a22bfa524e755475898e93df339d47d3a563bb31
Lazy-Chowhound <1103036472@qq.com>
2022-02-05 00:22:54 +0800

    增加多进程添加扰动

--- audio_python/Perturbation/NoisePattern.py
+++ audio_python/Perturbation/NoisePattern.py
@@ -1,4 +1,6 @@
-import soundfile
+import logging
+import threading
+from multiprocessing import Pool
 
 from Perturbation.AudioProcess import *
 from Util.Annotation import rpcApi
@@ -62,8 +64,7 @@ def add_natural_sounds(path, audioName, specificPattern):
     """
     sig, sr = librosa.load(path + audioName, sr=None)
     wavePath = path.replace("D:/AudioSystem/Audio/", "D:/AudioSystem/noiseAudio/")
-    specificPatterns = ["Wind", "Thunderstorm", "Water", "Fire"]
-    if specificPattern in specificPatterns:
+    if specificPattern in natural_sounds_specificPatterns:
         noiseSig, noise_sr = librosa.load("D:/AudioSystem/Noise/Natural Sounds/" + specificPattern + ".wav", sr=sr,
                                           mono=True)
         noiseAudio = addNoise(sig, noiseSig)
@@ -86,8 +87,7 @@ def add_animal(path, audioName, specificPattern):
     """
     sig, sr = librosa.load(path + audioName, sr=None)
     wavePath = path.replace("D:/AudioSystem/Audio/", "D:/AudioSystem/noiseAudio/")
-    specificPatterns = ["Pets", "Livestock", "Wild animals"]
-    if specificPattern in specificPatterns:
+    if specificPattern in animal_specificPatterns:
         noiseSig, noise_sr = librosa.load("D:/AudioSystem/Noise/Animal/" + specificPattern + ".wav", sr=sr,
                                           mono=True)
         noiseAudio = addNoise(sig, noiseSig)
@@ -110,9 +110,7 @@ def add_sound_of_things(path, audioName, specificPattern):
     """
     sig, sr = librosa.load(path + audioName, sr=None)
     wavePath = path.replace("D:/AudioSystem/Audio/", "D:/AudioSystem/noiseAudio/")
-    specificPatterns = ["Vehicle", "Engine", "Domestic sounds", "Bell", "Alarm", "Mechanisms", "Explosion", "Wood",
-                        "Glass", "Liquid", "Miscellaneous sources", "Specific impact sounds"]
-    if specificPattern in specificPatterns:
+    if specificPattern in sound_of_things_specificPatterns:
         noiseSig, noise_sr = librosa.load("D:/AudioSystem/Noise/Sound of things/" + specificPattern + ".wav", sr=sr,
                                           mono=True)
         noiseAudio = addNoise(sig, noiseSig)
@@ -135,9 +133,7 @@ def add_human_sounds(path, audioName, specificPattern):
     """
     sig, sr = librosa.load(path + audioName, sr=None)
     wavePath = path.replace("D:/AudioSystem/Audio/", "D:/AudioSystem/noiseAudio/")
-    specificPatterns = ["Human voice", "Whistling", "Respiratory sounds", "Human locomotion", "Hands", "Heartbeat",
-                        "Human group actions"]
-    if specificPattern in specificPatterns:
+    if specificPattern in human_sounds_specificPatterns:
         noiseSig, noise_sr = librosa.load("D:/AudioSystem/Noise/Human Sounds/" + specificPattern + ".wav", sr=sr,
                                           mono=True)
         noiseAudio = addNoise(sig, noiseSig)
@@ -160,8 +156,7 @@ def add_music(path, audioName, specificPattern):
     """
     sig, sr = librosa.load(path + audioName, sr=None)
     wavePath = path.replace("D:/AudioSystem/Audio/", "D:/AudioSystem/noiseAudio/")
-    specificPatterns = ["Musical instrument", "Music genre", "Musical concepts", "Music role", "Music mood"]
-    if specificPattern in specificPatterns:
+    if specificPattern in music_specificPatterns:
         noiseSig, noise_sr = librosa.load("D:/AudioSystem/Noise/Music/" + specificPattern + ".wav", sr=sr,
                                           mono=True)
         noiseAudio = addNoise(sig, noiseSig)
@@ -184,9 +179,7 @@ def add_source_ambiguous_sounds(path, audioName, specificPattern):
     """
     sig, sr = librosa.load(path + audioName, sr=None)
     wavePath = path.replace("D:/AudioSystem/Audio/", "D:/AudioSystem/noiseAudio/")
-    specificPatterns = ["Generic impact sounds", "Surface contact", "Deformable shell", "Onomatopoeia", "Silence",
-                        "Other sourceless"]
-    if specificPattern in specificPatterns:
+    if specificPattern in source_ambiguous_sounds_specificPatterns:
         noiseSig, noise_sr = librosa.load("D:/AudioSystem/Noise/Source-ambiguous sounds/" + specificPattern + ".wav",
                                           sr=sr, mono=True)
         noiseAudio = addNoise(sig, noiseSig)
@@ -210,3 +203,84 @@ def writeNoiseAudio(path, noiseAudioName, noiseAudio, sr):
     soundfile.write(path + noiseAudioName, noiseAudio, sr)
     trans_wav_to_mp3(path, noiseAudioName)
     removeAudio(path, noiseAudioName)
+
+
+def add_randomly_multiProcess(path):
+    """
+    多线程添加扰动
+    :param path: 形如 D:/AudioSystem/Audio/cv-corpus-chinese/clips/
+    :return:
+    """
+    logging.basicConfig(level=logging.ERROR, filename="error.log", filemode="a",
+                        format="%(levelname)s %(asctime)s %(filename)s %(message)s")
+    PROCESS_NUM = 8
+    audio_list = []
+    for root, dirs, files in os.walk(path):
+        audio_list = files
+    task_slice = math.ceil(len(audio_list) / PROCESS_NUM)
+
+    pool = Pool(PROCESS_NUM)
+    for i in range(0, PROCESS_NUM):
+        pool.apply_async(add_pattern_range,
+                         args=(path, audio_list, i * task_slice,
+                               min((i + 1) * task_slice - 1, len(audio_list) - 1),))
+    pool.close()
+    pool.join()
+
+
+def add_pattern_range(path, audio_list, start, end):
+    """
+    范围添加扰动
+    :param path:
+    :param audio_list:
+    :param start:
+    :param end:
+    :return:
+    """
+    print('process %s working...' % (os.getpid()))
+    for audio in audio_list[start:end]:
+        add_pattern_randomly(path, audio)
+
+
+def add_pattern_randomly(path, file):
+    """
+    随即个某一音频添加噪声
+    :param path: 形如 D:/AudioSystem/Audio/cv-corpus-chinese/clips/
+    :param file: common_voice_zh-CN_18524189.mp3
+    :return:
+    """
+    try:
+        p = random.randint(1, 8)
+        if p == 1:
+            add_gaussian_noise(path, file)
+        elif p == 2:
+            ptype = sound_level_specificPatterns
+            add_sound_level(path, file, ptype[random.randint(0, len(ptype) - 1)])
+        elif p == 3:
+            ptype = animal_specificPatterns
+            add_animal(path, file, ptype[random.randint(0, len(ptype) - 1)])
+        elif p == 4:
+            ptype = sound_of_things_specificPatterns
+            add_sound_of_things(path, file, ptype[random.randint(0, len(ptype) - 1)])
+        elif p == 5:
+            ptype = human_sounds_specificPatterns
+            add_human_sounds(path, file, ptype[random.randint(0, len(ptype) - 1)])
+        elif p == 6:
+            ptype = natural_sounds_specificPatterns
+            add_natural_sounds(path, file, ptype[random.randint(0, len(ptype) - 1)])
+        elif p == 7:
+            ptype = music_specificPatterns
+            add_music(path, file, ptype[random.randint(0, len(ptype) - 1)])
+        elif p == 8:
+            ptype = source_ambiguous_sounds_specificPatterns
+            add_source_ambiguous_sounds(path, file, ptype[random.randint(0, len(ptype) - 1)])
+        print(path + file + " finished")
+    except Exception as e:
+        logging.warning(path + file + " fail ", e)
+
+
+if __name__ == "__main__":
+    add_randomly_multiProcess("D:/AudioSystem/Audio/cv-corpus-arabic/clips")
+    add_randomly_multiProcess("D:/AudioSystem/Audio/cv-corpus-chinese/clips")
+    add_randomly_multiProcess("D:/AudioSystem/Audio/cv-corpus-portuguese/clips")
+    add_randomly_multiProcess("D:/AudioSystem/Audio/cv-corpus-russian/clips")
