00b2fbef7f98a222760b60cdb92003a44814dfad
Lazy-Chowhound <1103036472@qq.com>
2022-02-25 15:54:15 +0800

    修复无法正确获取音频的问题

--- audio_web/src/Util/Path.js
+++ audio_web/src/Util/Path.js
@@ -4,9 +4,9 @@ const datasetPath = baseUrl + "/Audio/"
 const noiseDatasetPath = baseUrl + "/NoiseAudio/"
 
 const clips_path = new Map([["cv-corpus-chinese", datasetPath + "cv-corpus-chinese/clips/"],
-    ["timit", datasetPath + "timit/lisa/data/timit/raw/TIMIT/TRAIN/"]])
+    ["timit", datasetPath + "timit/lisa/data/timit/raw/TIMIT/"]])
 
 const noise_clips_path = new Map([["cv-corpus-chinese", noiseDatasetPath + "cv-corpus-chinese/clips/"],
-    ["timit", noiseDatasetPath + "timit/lisa/data/timit/raw/TIMIT/TRAIN/"]])
+    ["timit", noiseDatasetPath + "timit/lisa/data/timit/raw/TIMIT/"]])
 
 export {clips_path, noise_clips_path}
