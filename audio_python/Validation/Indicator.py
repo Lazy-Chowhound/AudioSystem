919b7bf896c9b85d147367e1d2974d927962d3a2
Lazy-Chowhound <1103036472@qq.com>
2022-02-27 21:24:46 +0800

    修改注释

--- audio_python/Validation/Indicator.py
+++ audio_python/Validation/Indicator.py
@@ -14,7 +14,7 @@ def wer(r, h):
 def cer(r, h):
     """
     计算 CER，用于中文
-    :param r: 真实的 她马上要去吃午饭
+    :param r: 真实的 他马上要去吃午饭
     :param h: 推测的 我要去吃晚饭了
     :return:
     """
@@ -24,8 +24,8 @@ def cer(r, h):
 def wer_overall(r_list: list, h_list: list):
     """
     整个数据集的 WER
-    :param r_list:
-    :param h_list:
+    :param r_list: ["i can spell", "i hope"]
+    :param h_list: ["i kan cpell", "i hop"]
     :return:
     """
     return jiwer.wer(r_list, h_list)
@@ -34,8 +34,8 @@ def wer_overall(r_list: list, h_list: list):
 def cer_overall(r_list: list, h_list: list):
     """
     整个数据集的 CER
-    :param r_list:
-    :param h_list:
+    :param r_list: ["马上就来","你要去哪儿"]
+    :param h_list: ["马上来","你要去那儿"]
     :return:
     """
     return jiwer.cer(r_list, h_list)
