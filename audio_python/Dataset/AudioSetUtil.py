ea97fad138c3abcf012cd71722e441454f9f7e4a
Lazy-Chowhound <1103036472@qq.com>
2022-04-11 15:40:40 +0800

    重构程序结构

---
+++ audio_python/Dataset/AudioSetUtil.py
@@ -0,0 +1,89 @@
+import json
+
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+from pytube import YouTube
+
+
+def download_video_from_Youtube(url, path):
+    """
+    从Youtube上下载视频
+    :param url: 视频地址
+    :param path: 下载目录
+    :return:
+    """
+    yt = YouTube(url)
+    yt.streams.filter(file_extension="mp4").first().download(path)
+
+
+def extract_audio(source_path, start, end, pattern_type, target_path):
+    """
+    从视频中提取音频
+    :param source_path:
+    :param start: 开始的秒数
+    :param end: 结束的秒数
+    :param pattern_type:
+    :param target_path: 输出路径 形如 C:/Users/Nakano Miku/Desktop/audio/
+    :return:
+    """
+    audio_background = AudioFileClip(source_path).subclip(start, end)
+    audio_background.write_audiofile(target_path + pattern_type + ".wav", fps=48000)
+
+
+def load_ontology(path="C:/Users/Nakano Miku/Desktop/毕业论文/资料/ontology-master/ontology.json"):
+    """
+    加载 json 文件
+    :param path: json文件地址
+    :return:
+    """
+    with open(path, 'r', encoding='utf8') as f:
+        json_data = json.load(f)
+    return json_data
+
+
+def get_json_from_id(json_id, json_data):
+    """
+    根据id获取json字典
+    :param json_id:
+    :param json_data:
+    :return:
+    """
+    for item in json_data:
+        if json_id == item['id']:
+            return item
+
+
+def get_child(url_dict: dict, item: dict, name, json_data):
+    """
+    递归获取所有子孩子下的 url
+    :param url_dict: url字典
+    :param item: 当前json字典
+    :param name: 名称
+    :param json_data: 原始json
+    :return:
+    """
+    examples = item['positive_examples']
+    if len(name) == 0:
+        cur_name = item['name']
+    else:
+        cur_name = name + "_" + item['name']
+    if len(examples) != 0:
+        if cur_name not in url_dict.keys():
+            url_dict[cur_name] = []
+        for ex in examples:
+            url_dict[cur_name].append(ex)
+    children = item['child_ids']
+    if len(children) != 0:
+        for child in children:
+            child_item = get_json_from_id(child, json_data)
+            get_child(url_dict, child_item, cur_name, json_data)
+
+
+def get_url_dict():
+    url_dict = {}
+    json_data = load_ontology()
+    categories = ["Human sounds", "Animal", "Natural sounds", "Source-ambiguous sounds", "Sounds of things", "Music"]
+    for category in categories:
+        for item in json_data:
+            if item['name'] == category:
+                get_child(url_dict, item, "", json_data)
+    return url_dict
---
+++ audio_python/Validation/Audio Quality.py
@@ -0,0 +1,146 @@
+import librosa
+import pysepm
+from matplotlib import pyplot as plt
+from pesq import pesq
+from pysepm import stoi
+
+from Dataset.CommonVoiceDataset.CommonVoiceDataset import CommonVoiceDataset
+from Util.AudioUtil import pattern_to_name, pattern_types_dict, get_pattern_info_from_name
+
+
+def get_pesq(clean_audio, noise_audio):
+    """
+    计算两个音频的 pesq,范围[-0.5,4.5]，越高越好
+    :param clean_audio: 原始文件
+    :param noise_audio: 待评估文件
+    :return: score
+    """
+    clean_sig, sr1 = librosa.load(clean_audio, sr=16000)
+    noise_sig, sr2 = librosa.load(noise_audio, sr=16000)
+    pseq_score = pesq(sr1, clean_sig, noise_sig, 'wb')
+    return pseq_score
+
+
+def get_WSS(clean_audio, noise_audio):
+    """
+    计算语音的 WSS 值，值越小越好
+    :param clean_audio:
+    :param noise_audio:
+    :return:
+    """
+    clean_sig, sr1 = librosa.load(clean_audio, sr=None)
+    noise_sig, sr2 = librosa.load(noise_audio, sr=None)
+    wss_score = pysepm.wss(clean_sig, noise_sig, sr1)
+    return wss_score
+
+
+def get_stoi(clean_audio, noise_audio):
+    """
+    计算语音的 STOI 值，范围[0,1]，值越大，可懂度越高.
+    :param clean_audio:
+    :param noise_audio:
+    :return:
+    """
+    clean_sig, sr1 = librosa.load(clean_audio, sr=None)
+    noise_sig, sr2 = librosa.load(noise_audio, sr=None)
+    stoi_score = stoi(clean_sig, noise_sig, sr1, extended=False)
+    return stoi_score
+
+
+def get_NCM(clean_audio, noise_audio):
+    """
+    计算语音的 NCM 值
+    :param clean_audio:
+    :param noise_audio:
+    :return:
+    """
+    clean_sig, sr1 = librosa.load(clean_audio, sr=16000)
+    noise_sig, sr2 = librosa.load(noise_audio, sr=16000)
+    ncm_score = pysepm.ncm(clean_sig, noise_sig, sr1)
+    return ncm_score
+
+
+def get_pattern_types_dict():
+    """
+    返回 common voice 扰动的列表
+    :return: {"Gaussian noise":[[原音频,扰动音频],[]...]},
+              "Sound level":{"Louder":[[],[]...],
+                             "Quieter":[[],[]...],
+                             ...
+                             }
+              ...
+              }
+    """
+    p_t_dict = {}
+    for item in pattern_to_name.keys():
+        if item == "Gaussian noise":
+            p_t_dict[item] = []
+        else:
+            p_t_dict[item] = {}
+            for sub_item in pattern_types_dict.get(item):
+                p_t_dict[item][sub_item] = []
+    cvd = CommonVoiceDataset("cv-corpus-chinese")
+    noise_audios = cvd.get_noise_audio_clips_list()
+    for noise_audio in noise_audios:
+        audio, pattern_tag = cvd.get_name_and_pattern_tag(noise_audio)
+        pattern, pattern_type = get_pattern_info_from_name(pattern_tag)
+        if pattern == "Gaussian noise":
+            p_t_dict[pattern].append([audio, noise_audio])
+        else:
+            p_t_dict[pattern][pattern_type].append([audio, noise_audio])
+    return p_t_dict
+
+
+def draw_quality_and_intelligibility_chart():
+    """
+    common voice 音频质量和可懂度图
+    :return:
+    """
+    p_t_dict = get_pattern_types_dict()
+    clean_wavs = []
+    noise_wavs = []
+    for item in p_t_dict.keys():
+        if type(p_t_dict.get(item)).__name__ == 'list':
+            clean_wavs.append(p_t_dict[item][0][0])
+            noise_wavs.append(p_t_dict[item][0][1])
+        else:
+            for sub_item in p_t_dict.get(item).keys():
+                if sub_item != "Speed" and len(p_t_dict[item][sub_item]) != 0:
+                    clean_wavs.append(p_t_dict[item][sub_item][0][0])
+                    noise_wavs.append(p_t_dict[item][sub_item][0][1])
+    pesq_score = []
+    stoi_score = []
+    wss_score = []
+    ncm_score = []
+    x = []
+    for i in range(0, len(clean_wavs)):
+        pesq_score.append(get_pesq(clean_wavs[i], noise_wavs[i]))
+        stoi_score.append(get_stoi(clean_wavs[i], noise_wavs[i]))
+        wss_score.append(get_WSS(clean_wavs[i], noise_wavs[i]))
+        ncm_score.append(get_NCM(clean_wavs[i], noise_wavs[i]))
+        x.append(i + 1)
+
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.plot(x, pesq_score, label="PESQ")
+    plt.plot(x, stoi_score, label="STOI")
+    plt.plot(x, ncm_score, label="NCM")
+    plt.legend()
+    plt.xlabel("音频序号")
+    plt.ylabel("分数")
+    plt.show()
+
+
+def draw_wer_chart():
+    x = ["wav2vec2-\nlarge-960h", "wav2vec2-\nlarge-lv60-\ntimit-asr", "wav2vec2-\nbase-timit-\nasr",
+         "s2t-small-\nlibrispeech-\nasr",
+         "s2t-medium-\nlibrispeech-\nasr", "s2t-large-\nlibrispeech-\nasr"]
+    y1 = [0.12667034026725443, 0.1386534353249259, 0.2555992006064365, 0.10778030459651299, 0.11412032251395493,
+          0.10281855144373234]
+    y2 = [0.5199752031960325, 0.604024533112811, 0.7269657501205982, 1.0998552821997105, 1.0808352284473848,
+          1.061953001171525]
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.plot(x, y1, label="原wer")
+    plt.plot(x, y2, label="现wer")
+    plt.legend()
+    plt.ylabel("wer")
+    plt.show()
