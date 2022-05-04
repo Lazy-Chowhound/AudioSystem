3f9214c8a0cee480f648c73488fa445af500cc51
Lazy-Chowhound <1103036472@qq.com>
2022-05-04 15:49:59 +0800

    TUDA数据集增加部分功能

--- audio_python/Dataset/TUDA/TUDA.py
+++ audio_python/Dataset/TUDA/TUDA.py
@@ -1,7 +1,7 @@
 import glob
 
 from Dataset.Dataset import Dataset
-from Util.AudioUtil import AUDIO_SETS_PATH, NOISE_AUDIO_SETS_PATH, get_audio_form
+from Util.AudioUtil import AUDIO_SETS_PATH, NOISE_AUDIO_SETS_PATH, get_audio_form, get_str_n_find
 
 
 class TUDA(Dataset):
@@ -11,6 +11,15 @@ class TUDA(Dataset):
         self.clips_path = AUDIO_SETS_PATH + dataset + "/"
         self.noise_clips_path = NOISE_AUDIO_SETS_PATH + dataset + "/"
 
+    def get_name_and_pattern_tag(self, name):
+        """
+        从扰动名字中获取原本的名字和扰动标签
+        :param name: 扰动音频名称，
+        :return:
+        """
+        name = name.replace("\\", "/")
+        return name[:get_str_n_find(name, "_", 2)] + ".wav", name[get_str_n_find(name, "_", 2) + 1:name.rfind(".")]
+
     def get_testset_audio_clips_list(self):
         testset_path = self.clips_path + "test/"
         audio_list = glob.glob(testset_path + "*.wav")
@@ -18,4 +27,17 @@ class TUDA(Dataset):
         for audio in audio_list:
             if get_audio_form(audio) == "wav":
                 audios.append(audio.replace("\\", "/").replace(self.clips_path, ""))
+        return audios
+
+    def get_noise_testset_audio_clips_list(self):
+        """
+        获取扰动测试集
+        :return:
+        """
+        noise_testset_path = self.noise_clips_path + "test/"
+        audio_list = glob.glob(noise_testset_path + "*.wav")
+        audios = []
+        for audio in audio_list:
+            if get_audio_form(audio) == "wav":
+                audios.append(audio.replace("\\", "/").replace(self.noise_clips_path, ""))
         return audios
