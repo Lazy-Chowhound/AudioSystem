97af8a09c0b8d5dcd55107cb8e320111b95d6bd3
Lazy-Chowhound <1103036472@qq.com>
2022-05-09 22:34:05 +0800

    修改测试用例

--- audio_java/src/test/java/szp/audio/audio_java/TokenTest.java
+++ audio_java/src/test/java/szp/audio/audio_java/TokenTest.java
@@ -11,23 +11,13 @@ import org.junit.jupiter.api.Test;
 import java.util.Date;
 
 public class TokenTest {
-
     @Test
-    public void creatWrongUserToken() {
-        System.out.println(createUserToken("szp", "0", DateField.DAY_OF_YEAR));
-    }
-
-    @Test
-    public void creatWrongTimeToken() {
-        System.out.println(createUserToken("szp", "061210", DateField.SECOND));
-    }
-
-    public String createUserToken(String userName, String password, DateField dateField) {
-        Date expireDay = DateUtil.offset(new Date(), dateField, 7);
+    public void createUserToken() {
+        Date expireDay = DateUtil.offset(new Date(), DateField.DAY_OF_YEAR, 7);
         Algorithm algorithm = Algorithm.HMAC512("szp061210");
         JWTCreator.Builder builder = JWT.create();
-        return builder.withClaim("userName", userName).withClaim("password", password)
+        System.out.println(builder.withClaim("userName", "szp").withClaim("password", "061210")
                 .withExpiresAt(expireDay)
-                .sign(algorithm);
+                .sign(algorithm));
     }
 }
